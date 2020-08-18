3d75c33803a5ad195a589c54e515a53b6f597be6
unknown <anhdung.dau@gmail.com>
2020-08-18 12:34:17 +1000

    add link to authors & books

--- index.php
+++ index.php
@@ -1,22 +1,30 @@
 <?php
-session_start(); 
+  session_start();
+  include_once 'connection.php';
 ?>
 
-
-
-<?php 
-$con=mysqli_connect("localhost","root","","bookshelf");
-// Check connection
-if (mysqli_connect_errno($con))   {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-} 
-$sql = "SELECT title, url, author, books.authorID FROM books, authors WHERE books.authorID = authors.authorID order by title";
-$result = mysqli_query($con,$sql); 
-//echo mysqli_num_rows($result);
-while($row = mysqli_fetch_array($result)) {
-	echo '<a href="'.$row['url'].'" target="_blank">'.$row['title'].'</a> by ';
-	echo '<a href="authorbooks.php?authorID='.$row['authorID'].'">'.$row['author'].'</a>';
-	echo '<br>';
-}
-mysqli_close($con);  
-?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bookshelf</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+    <div class="w3-container w3-green">
+        <h1>Welcome to my Bookshelf</h1>
+    </div>
+    <div class="w3-row-padding">
+        <div class="w3-half">
+            <h2><a href="showauthors.php">List of Authors</a></h2>
+            <img src="img/author.jpg" style="max-width:100%;height:auto;">
+        </div>
+        <div class="w3-half">
+            <h2><a href="showbooks.php">List of Books</a></h2>
+            <img src="img/bookshelf.jpg" style="max-width:100%;height:auto;">
+        </div>
+    </div>
+</body>
+</html>
