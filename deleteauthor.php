7c7dec1bfb0acdd76d3966c9f0cda4ac2c5b8b0e
unknown <anhdung.dau@gmail.com>
2020-08-18 11:45:10 +1000

    add confirmation after deleting

--- deleteauthor.php
+++ deleteauthor.php
@@ -1,15 +1,13 @@
 <?php
-
-$con = mysqli_connect("localhost","root","","bookshelf");
-
-$authorID = mysqli_real_escape_string($con,$_GET['authorID']);
-
-$sql = "DELETE FROM books WHERE authorID = ".$authorID;
-$result=mysqli_query($con,$sql);
-
-$sql = "DELETE FROM authors WHERE authorID = ".$authorID;
-$result=mysqli_query($con,$sql);
-
-header('Location: showbooks.php');
-
+  session_start();
+  include_once 'connection.php';
+  $con = mysqli_connect("localhost","root","","bookshelf");
+  $authorID = mysqli_real_escape_string($con,$_GET['authorID']); 
+  $sql = "DELETE FROM books WHERE authorID = ".$authorID;
+  $result=mysqli_query($con,$sql);
+  $sql = "DELETE FROM authors WHERE authorID = ".$authorID;
+  $result=mysqli_query($con,$sql);
+  echo "Author and related books have been deleted successfully!"."<br>";
+  header( "refresh:2;url=showauthors.php" );
+  echo 'You will be redirect to the Authors List shortly';
 ?>
