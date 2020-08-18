e025cb879cc005165df4fa917f142d1a034e29a1
unknown <anhdung.dau@gmail.com>
2020-08-18 22:25:52 +1000

    add a connection to all files

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'bookshelf';
-$con=mysqli_connect($host,$user,$password,$database);
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $database = 'bookshelf';
+    $con=mysqli_connect($host,$user,$password,$database);
 ?>
