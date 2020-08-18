f0c8ec45dc15f8216d3ee2c73d74079d11b14ee7
unknown <anhdung.dau@gmail.com>
2020-08-18 11:44:00 +1000

    add css to addauthor

--- addauthor.php
+++ addauthor.php
@@ -1,12 +1,10 @@
 <?php
-$con = mysqli_connect("localhost","root","","bookshelf");
-
-$author = $_POST['authorname'];
-
-$sql = "INSERT INTO authors (authorID, author) VALUES (NULL, '$author')";
-
-$result=mysqli_query($con,$sql);
-
-echo "Author added: ".$author;
-
+  session_start();
+  include_once 'connection.php';
+  $author = $_POST['authorname'];
+  $sql = "INSERT INTO authors (authorID, author) VALUES (NULL, '$author')";
+  $result=mysqli_query($con,$sql);
+  echo "Author .$author. has been added successfully!";
+  header( "refresh:3;url=showauthors.php" );
+  echo 'You will be redirect to the Authors List shortly';
 ?>
